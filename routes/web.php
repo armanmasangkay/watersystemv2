494130777c73e6413201dbe1d6f5704c8dc73d75
armanmasangkay <vss.arman@gmail.com>
2021-03-02 13:58:32 +0800

    remove laravel root page

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,18 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return redirect(route('admin.dashboard'));
+})->middleware('auth');
 
 
 Route::get('/login',[LoginController::class,'index'])->name('login');
 Route::post('/login',[LoginController::class,'authenticate']);
 
 Route::prefix('admin')->name('admin.')->group(function(){
+
+ 
 
     Route::get('/register-customer',[CustomerController::class,'index'])
         ->middleware('auth')
