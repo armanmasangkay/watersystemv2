11b02613ceab0a27b05503ed50594f02328c2b59
miyunecadz <jvcadz@gmail.com>
2021-05-03 10:49:10 +0800

    Add building inspection functionality(approve and reject)

--- app/Http/Controllers/WaterWorksApprovalController.php
+++ app/Http/Controllers/WaterWorksApprovalController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Transaction;
 
 class WaterWorksApprovalController extends Controller
 {
     public function index()
     {
-        return view('pages.waterworks-request-approval', ['route' => 'admin.waterworks-request-approvals', 'search_heading' => 'SEARCH REQUEST']);
+        $transactions = Transaction::where('status', 'bldg_approved')->paginate(20);
+        return view('pages.waterworks-request-approval', ['route' => 'admin.waterworks-request-approvals', 'search_heading' => 'SEARCH REQUEST', 'transactions' => $transactions]);
     }
 }
