8b3bc06fe2f4372b2fe5c28529422336c36b3ea4
miyunecadz <jvcadz@gmail.com>
2021-07-13 13:49:44 +0800

    Add LedgerTransaction Feature Test

---
+++ tests/Feature/LedgerTransactionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LedgerTransactionTest extends TestCase
+{
+    public function test_success_adding_new_ledger_transaction(){
+
+    }
+}
