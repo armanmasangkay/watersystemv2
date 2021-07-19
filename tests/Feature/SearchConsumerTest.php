be38eaeefddab41fbec231c46ac4ebd5d1f4a586
armanmasangkay <vss.arman@gmail.com>
2021-07-19 09:47:45 +0800

    remove unused imports on searchconsumertest

--- tests/Feature/SearchConsumerTest.php
+++ tests/Feature/SearchConsumerTest.php
@@ -4,8 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class SearchConsumerTest extends TestCase
