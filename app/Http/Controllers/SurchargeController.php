3c2c67dca31357bc9bbf50e362404e542b6a849b
miyunecadz <jvcadz@gmail.com>
2021-07-20 09:13:32 +0800

    Remove commented codes

--- app/Http/Controllers/SurchargeController.php
+++ app/Http/Controllers/SurchargeController.php
@@ -26,9 +26,6 @@ class SurchargeController extends Controller
 
         $rate = $request->surcharge_rate? $request->surcharge_rate : '0';
 
-        // $Getsurcharge = Surcharge::all();
-        // $surcharge = Surcharge::find($Getsurcharge[0]->id);
-
         $surcharge = Surcharge::where('id', '!=', '')->first();
 
         $surcharge->rate = $rate / 100;
