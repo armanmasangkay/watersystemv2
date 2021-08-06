3831b64629ea639a9854c6c93788af727d6ca75f
armanmasangkay <vss.arman@gmail.com>
2021-08-06 20:11:48 +0800

    fix some tests

--- tests/Feature/RatesUpdateTest.php
+++ tests/Feature/RatesUpdateTest.php
@@ -12,18 +12,28 @@ use Tests\TestCase;
 class RatesUpdateTest extends TestCase
 {
     use RefreshDatabase;
-    public function test_success_if_surcharge_update_with_valid_data(){
+    public function test_success_if_surcharge_update_with_valid_data()
+    {
         $user = User::factory()->create();
         Surcharge::factory()->create(['rate'=>'0.2']);
         $response = $this->actingAs($user)->post(route('admin.surcharge-update'),['surcharge_rate' => 10]);
         $this->assertDatabaseHas('surcharges', ['rate' => 0.1]);
         $response->assertJson(['updated' => true]);
     }
-    public function test_fail_if_surcharge_update_with_invalid_data(){
+    public function test_fail_if_surcharge_update_with_invalid_data()
+    {
         $user = User::factory()->create();
         Surcharge::factory()->create(['rate'=>'0.2']);
         $response = $this->actingAs($user)->post(route('admin.surcharge-update'),['surcharge_rate' => "sample"]);
         $response->assertJson(['updated' => false]);
     }
+    public function test_fail_if_surcharge_is_negative()
+    {
+        $user = User::factory()->create();
+        Surcharge::factory()->create(['rate'=>'0.2']);
+        $response = $this->actingAs($user)->post(route('admin.surcharge-update'),['surcharge_rate' => "-1"]);
+        $response->assertJson(['updated' => false]);
+    }
+
 
 }
