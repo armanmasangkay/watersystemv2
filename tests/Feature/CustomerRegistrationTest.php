b01bbbc9262bf091a6ddfea74e6b3f1af81c6b91
armanmasangkay <vss.arman@gmail.com>
2021-08-06 11:28:22 +0800

    add more test to customer registration

--- tests/Feature/CustomerRegistrationTest.php
+++ tests/Feature/CustomerRegistrationTest.php
@@ -27,6 +27,186 @@ class CustomerRegistrationTest extends TestCase
             ]);
     }
 
+
+
+    public function test_organization_name_is_saved_if_field_is_not_empty()
+    {
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $date = Carbon::now();
+        $response = $this->post(route('admin.register-customer.store'),[
+            'org_name'=>'SLSU',
+            'firstname' => "June Vic",
+            'middlename' => '',
+            'lastname' => 'Cadayona',
+            'civil_status' => 'single',
+            'purok' => 'Somewhere',
+            'barangay' => 'Somewhere',
+            'contact_number' => '09178781045',
+            'connection_type' => 'Residential',
+            'connection_status' => 'Active',
+            'purchase_option' => 'cash',
+            'reading_meter' => '100',
+            'balance' => '100',
+            'reading_date' => $date->toDateString(),
+            'billing_meter_ips' => '100'
+        ]);
+        $this->assertDatabaseCount('customers', 1);
+        $this->assertDatabaseHas('customers',['org_name'=>'SLSU']);
+        $response->assertJson(['created'=> true]);
+
+    }
+
+    public function test_should_error_if_conn_type_is_others_but_have_not_specified_anything()
+    {
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $date = Carbon::now();
+        $response = $this->post(route('admin.register-customer.store'),[
+            'org_name'=>'',
+            'firstname' => "June Vic",
+            'middlename' => '',
+            'lastname' => 'Cadayona',
+            'civil_status' => 'single',
+            'purok' => 'Somewhere',
+            'barangay' => 'Somewhere',
+            'contact_number' => '09178781045',
+            'connection_type' => 'others',
+            'connection_status' => 'Active',
+            'purchase_option' => 'cash',
+            'reading_meter' => '100',
+            'balance' => '100',
+            'reading_date' => $date->toDateString(),
+            'billing_meter_ips' => '100'
+        ]);
+
+        $response->assertJson(['created'=> false]);
+        $response->assertJsonValidationErrors(['connection_type_specifics']);
+    }
+
+    public function test_should_not_produce_error_if_conn_type_is_others_but_have__specified_something()
+    {
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $date = Carbon::now();
+        $response = $this->post(route('admin.register-customer.store'),[
+            'org_name'=>'',
+            'firstname' => "June Vic",
+            'middlename' => '',
+            'lastname' => 'Cadayona',
+            'civil_status' => 'single',
+            'purok' => 'Somewhere',
+            'barangay' => 'Somewhere',
+            'contact_number' => '09178781045',
+            'connection_type' => 'others',
+            'connection_type_specifics'=>'some type',
+            'connection_status' => 'Active',
+            'purchase_option' => 'cash',
+            'reading_meter' => '100',
+            'balance' => '100',
+            'reading_date' => $date->toDateString(),
+            'billing_meter_ips' => '100'
+        ]);
+
+        $response->assertJson(['created'=> true]);
+        $response->assertJsonMissingValidationErrors(['connection_type_specifics']);
+    }
+
+
+    public function test_should_error_if_conn_status_is_others_but_have_not_specified_anything()
+    {
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $date = Carbon::now();
+        $response = $this->post(route('admin.register-customer.store'),[
+            'org_name'=>'',
+            'firstname' => "June Vic",
+            'middlename' => '',
+            'lastname' => 'Cadayona',
+            'civil_status' => 'single',
+            'purok' => 'Somewhere',
+            'barangay' => 'Somewhere',
+            'contact_number' => '09178781045',
+            'connection_type' => 'Residential',
+            'connection_status' => 'others',
+            'purchase_option' => 'cash',
+            'reading_meter' => '100',
+            'balance' => '100',
+            'reading_date' => $date->toDateString(),
+            'billing_meter_ips' => '100'
+        ]);
+
+        $response->assertJson(['created'=> false]);
+        $response->assertJsonValidationErrors(['connection_status_specifics']);
+    }
+
+    public function test_should_not_produce_error_if_conn_status_is_others_but_have__specified_something()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $date = Carbon::now();
+        $response = $this->post(route('admin.register-customer.store'),[
+            'org_name'=>'',
+            'firstname' => "June Vic",
+            'middlename' => '',
+            'lastname' => 'Cadayona',
+            'civil_status' => 'single',
+            'purok' => 'Somewhere',
+            'barangay' => 'Somewhere',
+            'contact_number' => '09178781045',
+            'connection_type' => 'others',
+            'connection_type_specifics'=>'some type',
+            'connection_status' => 'others',
+            'connection_status_specifics' => 'some status',
+            'purchase_option' => 'cash',
+            'reading_meter' => '100',
+            'balance' => '100',
+            'reading_date' => $date->toDateString(),
+            'billing_meter_ips' => '100'
+        ]);
+
+        $response->assertJson(['created'=> true]);
+        $response->assertJsonMissingValidationErrors(['connection_types_status']);
+    }
+
+    public function test_should_not_fail_even_if_org_name_is_empty()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $date = Carbon::now();
+        $response = $this->post(route('admin.register-customer.store'),[
+            'org_name'=>'',
+            'firstname' => "June Vic",
+            'middlename' => '',
+            'lastname' => 'Cadayona',
+            'civil_status' => 'single',
+            'purok' => 'Somewhere',
+            'barangay' => 'Somewhere',
+            'contact_number' => '09178781045',
+            'connection_type' => 'Residential',
+            'connection_status' => 'Active',
+            'purchase_option' => 'cash',
+            'reading_meter' => '100',
+            'balance' => '100',
+            'reading_date' => $date->toDateString(),
+            'billing_meter_ips' => '100'
+        ]);
+        $this->assertDatabaseCount('customers', 1);
+        $response->assertJson(['created'=> true]);
+
+    }
+
+
+
+    public function test_route_cannot_be_accessed_if_user_is_not_logged_in(){
+        $response = $this->get(route('admin.existing-customers.create'));
+        $response->assertRedirect(route('login'));
+    }
+
     public function test_fail_to_register_if_customer_with_incomplete_data(){
         $user = User::factory()->create();
         $response = $this->actingAs($user)->post(route('admin.register-customer.store'),[
@@ -98,5 +278,111 @@ class CustomerRegistrationTest extends TestCase
 
         $this->assertDatabaseCount('customers', 1);
         $response->assertJson(['created'=> true]);
+    }
+
+    public function test_previous_meter_reading_should_not_accept_negative_numbers()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $date = Carbon::now();
+        $response = $this->post(route('admin.register-customer.store'),[
+            'firstname' => "June Vic",
+            'middlename' => '',
+            'lastname' => 'Cadayona',
+            'civil_status' => 'single',
+            'purok' => 'Somewhere',
+            'barangay' => 'Somewhere',
+            'contact_number' => '09178781045',
+            'connection_type' => 'Residential',
+            'connection_status' => 'Active',
+            'purchase_option' => 'cash',
+            'reading_meter' => '-1',
+            'balance' => '100',
+            'reading_date' => $date->toDateString(),
+            'billing_meter_ips' => '100'
+        ]);
+
+        $response->assertJson(['created'=> false]);
+        $response->assertJsonValidationErrors(['reading_meter']);
+    }
+
+    public function test_balance_should_not_accept_negative_numbers()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $date = Carbon::now();
+        $response = $this->post(route('admin.register-customer.store'),[
+            'firstname' => "June Vic",
+            'middlename' => '',
+            'lastname' => 'Cadayona',
+            'civil_status' => 'single',
+            'purok' => 'Somewhere',
+            'barangay' => 'Somewhere',
+            'contact_number' => '09178781045',
+            'connection_type' => 'Residential',
+            'connection_status' => 'Active',
+            'purchase_option' => 'cash',
+            'reading_meter' => '0',
+            'balance' => '-100',
+            'reading_date' => $date->toDateString(),
+            'billing_meter_ips' => '100'
+        ]);
+
+        $response->assertJson(['created'=> false]);
+        $response->assertJsonValidationErrors(['balance']);
+    }
+
+    public function test_meter_IPS_should_not_accept_negative_numbers()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $date = Carbon::now();
+        $response = $this->post(route('admin.register-customer.store'),[
+            'firstname' => "June Vic",
+            'middlename' => '',
+            'lastname' => 'Cadayona',
+            'civil_status' => 'single',
+            'purok' => 'Somewhere',
+            'barangay' => 'Somewhere',
+            'contact_number' => '09178781045',
+            'connection_type' => 'Residential',
+            'connection_status' => 'Active',
+            'purchase_option' => 'cash',
+            'reading_meter' => '0',
+            'balance' => '100',
+            'reading_date' => $date->toDateString(),
+            'billing_meter_ips' => '-100'
+        ]);
+
+        $response->assertJson(['created'=> false]);
+        $response->assertJsonValidationErrors(['billing_meter_ips']);
+    }
+
+    public function test_middle_name_if_saved_if_provided()
+    {
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $date = Carbon::now();
+        $response = $this->post(route('admin.register-customer.store'),[
+            'firstname' => "June Vic",
+            'middlename' => 'Meowk',
+            'lastname' => 'Cadayona',
+            'civil_status' => 'single',
+            'purok' => 'Somewhere',
+            'barangay' => 'Somewhere',
+            'contact_number' => '09178781045',
+            'connection_type' => 'Residential',
+            'connection_status' => 'Active',
+            'purchase_option' => 'cash',
+            'reading_meter' => '0',
+            'balance' => '100',
+            'reading_date' => $date->toDateString(),
+            'billing_meter_ips' => '100'
+        ]);
+
+        $response->assertJson(['created'=> true]);
+        $this->assertDatabaseCount('customers',1);
+       
     }
 }
