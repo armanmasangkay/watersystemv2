122b1ae6e68ed2b207fef1cd0aa5decdac2dcdb7
miyunecadz <jvcadz@gmail.com>
2021-08-25 20:53:18 +0800

    Status

--- app/Classes/Facades/ConnectionTypeHelper.php
+++ app/Classes/Facades/ConnectionTypeHelper.php
@@ -5,13 +5,8 @@ namespace App\Classes\Facades;
 class ConnectionTypeHelper{
     public static function toReadableString($value){
         $data = [
-            'new_connection' => 'New Connection',
-            'reconnection'=>'Reconnection',
-            'transfer_of_meter'=>'Transfer of Meter',
-            'change_of_meter'=>'Change of Meter',
-            'transfer_of_ownership'=>'Transfer of Ownership',
-            'disconnection'=>'Disconnection',
-            'others'=>'Others',
+            'pending_building_inspection' => 'Pending for Building Inspection',
+            'pending_waterworks_inspection'=>'Pending for Waterworks  Inspection',
             'pending' => 'Pending'
         ];
 
