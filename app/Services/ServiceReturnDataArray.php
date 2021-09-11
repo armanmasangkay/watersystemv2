09e04cc2c8b0b7eafbf0e1cb5730dfb72afe579e
armanmasangkay <vss.arman@gmail.com>
2021-09-11 10:13:40 +0800

    add show all option

--- app/Services/ServiceReturnDataArray.php
+++ app/Services/ServiceReturnDataArray.php
@@ -7,10 +7,33 @@ class ServiceReturnDataArray{
 
     public static function set($serviceStatus, $services){
         $data = [
-            'pending_building_inspection' => ['search_route' => 'admin.search','route' => 'admin.undo', 'text' => ['Lists of Request for Building/Area Inspections', 'View Approved Request'], 'search_heading' => 'SEARCH REQUEST','services' => $services],
-            'denied_building_inspection' =>['search_route' => 'admin.search.denied','route' => 'admin.request-approvals', 'text' => ['Lists of Denied Request for Building/Area Inspections', 'Return Back'], 'search_heading' => 'SEARCH REQUEST','services' => $services],
-            'pending_waterworks_inspection' => ['search_route' => 'admin.water.search','route' => 'admin.waterworks-request-approvals', 'search_heading' => 'SEARCH REQUEST', 'services' => $services],
-            'pending_engineer_approval' => ['search_route' => 'admin.municipal-engineer.search','route' => 'admin.waterworks-request-approvals', 'search_heading' => 'SEARCH REQUEST', 'services' => $services]
+            'pending_building_inspection' => [
+                'search_route' => 'admin.search',
+                'route' => 'admin.undo',
+                'text' => ['Lists of Request for Building/Area Inspections','View Approved Request'],
+                'search_heading' => 'SEARCH REQUEST',
+                'services' => $services
+            ],
+
+            'denied_building_inspection' =>[
+                'search_route' => 'admin.search.denied',
+                'route' => 'admin.request-approvals',
+                'text' => ['Lists of Denied Request for Building/Area Inspections', 'Return Back'],
+                'search_heading' => 'SEARCH REQUEST',
+                'services' => $services
+            ],
+            'pending_waterworks_inspection' => [
+                'search_route' => 'admin.water.search',
+                'route' => 'admin.waterworks-request-approvals',
+                'search_heading' => 'SEARCH REQUEST', 'services' => $services
+            ],
+            'pending_engineer_approval' => [
+                'search_route' => 'admin.municipal-engineer.search',
+                'index_route'=>'admin.municipal-engineer.index',
+                'route' => 'admin.waterworks-request-approvals',
+                'search_heading' => 'SEARCH REQUEST',
+                'services' => $services,
+            ]
         ];
         return $data[$serviceStatus];
     }
