b68597ed26396391703728d3f9652c05edfd8d10
armanmasangkay <vss.arman@gmail.com>
2021-10-01 09:33:33 +0800

    update tests

---
+++ tests/Feature/controllers/UserPasswordControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Controllers\Feature;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserPasswordControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
