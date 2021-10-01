ed41ee1ba7405bec7d99b326360015a6c3d859e5
armanmasangkay <vss.arman@gmail.com>
2021-10-01 10:33:12 +0800

    remove unnecessary comments

--- app/Http/Controllers/WaterWorksApprovalController.php
+++ app/Http/Controllers/WaterWorksApprovalController.php
@@ -23,13 +23,6 @@ class WaterWorksApprovalController extends Controller
         return view('pages.waterworks-request-approval', ServiceReturnDataArray::set('pending_waterworks_inspection', $services));
     }
 
-    // public function search_denied(Request $request){
-    //     $services = (new ServicesFromKeyword)->get($request->account_number, 'denied_waterworks_inspection');
-    //     $services = new Paginator($services->all(), 10);
-
-    //     // dd($services);
-    //     return view('pages.waterworks-request-approval', ['search_route' => 'admin.water.search.denied','route' => 'admin.waterworks-request-approvals', 'services' => $services]);
-    // }
 
     public function approve(Request $request)
     {
@@ -42,8 +35,6 @@ class WaterWorksApprovalController extends Controller
     public function reject($id)
     {
         $service = Service::findOrFail($id);
-        // $services->status = "denied_waterworks_inspection";
-        // $services->save();
         $service->deny();
 
         return redirect(route('admin.waterworks-request-approvals'));
