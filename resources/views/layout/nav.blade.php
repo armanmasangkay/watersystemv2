5439e0da4764eb9d308665d7eaaef3baf0df1234
darksidebug <nobeginmasob@gmail.com>
2021-10-05 13:36:55 +0800

    change UI for dashboard cardtext and time ampm, setting on officer to set officer

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -86,7 +86,7 @@
               <i data-feather="settings" class="feather-16 m-1"></i> Settings
             </a>
             <ul class="dropdown-menu px-2" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="{{route('admin.officers.index')}}" >Officers</a></li>
+              <li><a class="dropdown-item" href="{{route('admin.officers.index')}}" >Set Officers</a></li>
               <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">Water Rates</a></li>
               <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#surchargeModel">Surcharge</a></li>
               <li><a class="dropdown-item" href="{{route('users.update-password.edit')}}">Change Password</a></li>
