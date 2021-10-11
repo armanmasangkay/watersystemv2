6cd61cdd65349354aa75c8b81f40cffba926e409
armanmasangkay <vss.arman@gmail.com>
2021-10-11 16:46:58 +0800

    add more tests

--- resources/views/pages/work-order-payments.blade.php
+++ resources/views/pages/work-order-payments.blade.php
@@ -5,6 +5,7 @@
 
 @section('content')
 
+@if($payments->count()>0)
 <h4 class="mt-4 text-center">Work Order Payments</h4>
 <table class="table table-striped mt-4">
     <thead>
@@ -15,19 +16,25 @@
         <th>Paid on</th>
       </thead>
       <tbody>
-          @foreach($payments as $payment)
-                <tr>
-                    <td>{{ $payment->service->prettyServiceType() }}</td>
-                    <td>{{ $payment->customer_id }}</td>
-                    <td>{{ $payment->or_no }}</td>
-                    <td>{{ $payment->payment_amount }}</td>
-                    <td>{{ $payment->created_at }}</td>
+       
+          @foreach ($payments as $payment)
+            <tr>
+                <td>{{ $payment->service->prettyServiceType() }}</td>
+                <td>{{ $payment->customer_id }}</td>
+                <td>{{ $payment->or_no }}</td>
+                <td>{{ $payment->payment_amount }}</td>
+                <td>{{ $payment->created_at }}</td>
 
-                </tr>
+            </tr>
+         
           @endforeach
-
+               
+      
       </tbody>
 </table>
+@else
+<h4 class="text-center">No payments to show!</h4>
+@endif
 
 @endsection
 
