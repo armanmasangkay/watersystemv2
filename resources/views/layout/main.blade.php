9ab0cfaa02c6e625fd23f375042f3dfc4840a7e2
darksidebug <nobeginmasob@gmail.com>
2023-06-09 08:42:06 -0700

    change title

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -2,7 +2,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ERP User Role Base Access - @yield('title')</title>
+        <title>Macrohon Water System - @yield('title')</title>
         <!-- <link rel="shortcut icon" href="{{ asset('assets/img/logo-macrohon.png') }}" type="image/x-icon"> -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
